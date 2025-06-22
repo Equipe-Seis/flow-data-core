b6dff9be1c2ec8fb87bb926582fffa4fef35c79f
devmdi <devweb.dic@gmail.com>
2025-06-22 18:30:26 -0300

    ajuste para index para login
    redireciona o index para login

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,12 @@
 <!-- pages/index.vue -->
 <template>
-  <v-card class="mx-auto" max-width="400" outlined>
-    <v-card-title>Bem-vindo</v-card-title>
-    <v-card-text>
-      Esta é uma aplicação Nuxt 3 com Vuetify 3.
-    </v-card-text>
-    <v-card-actions>
-      <v-btn color="primary" @click="sayHello">Clique aqui</v-btn>
-    </v-card-actions>
-  </v-card>
+  <div>Redirecionando...</div>
 </template>
 
 <script setup>
-function sayHello() {
-  alert('Olá do Nuxt + Vuetify!')
-}
+definePageMeta({
+  layout: false,
+  middleware: 'redirect'
+})
 </script>
+
