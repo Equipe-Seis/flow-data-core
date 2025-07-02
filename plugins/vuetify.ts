e4c51c209517310cea54c40bb6109963960e4971
GuilhermePaini <guihdpaini@gmail.com>
2025-07-01 23:15:46 -0300

    refactor: changed vuetify initial config

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,12 +1,17 @@
 // plugins/vuetify.ts
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 
 export default defineNuxtPlugin(nuxtApp => {
   const vuetify = createVuetify({
-    components,
-    directives,
+    ssr: true,
+    defaults: {
+      VBtn: {
+        style: [{ textTransform: 'none' }],
+      },
+    },
   })
 
   nuxtApp.vueApp.use(vuetify)
